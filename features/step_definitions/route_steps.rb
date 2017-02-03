20b5ff7f618d5da31cdea2a3ec00ec60657a692d
Maxim <karamba92@mail.ru>
2017-02-03 15:30:13 +0600

    #41 поправил все тесты, вернул удаленное

--- features/step_definitions/route_steps.rb
+++ features/step_definitions/route_steps.rb
@@ -9,11 +9,23 @@ end
 When(/^я перехожу на страницу почты mailcatcher$/) do
   visit('http://127.0.0.1:1080')
 end
-When(/^прикрепляю файл$/) do
+
+When(/^прикрепляю скан паспорта юр.лица$/) do
   page.attach_file("client_corporate_image", Rails.root + 'app/assets/images/beeline.png')
   sleep(0.5)
 end
+
 When(/^прикрепляю скан паспорта физ лица$/) do
   page.attach_file("client_individual_image", Rails.root + 'app/assets/images/megacom.png')
+  sleep(0.5)
+end
+
+When(/^кликаю на письмо через xpath$/) do
+  find(:xpath, "//*[@id='messages']/table/tbody/tr[1]/td[2]").click
+  sleep(0.5)
+end
+
+When(/^перехожу на страницу письма уведомления для физического лица$/) do
+  visit 'http://127.0.0.1:1080/messages/1.html'
   sleep(0.5)
 end
