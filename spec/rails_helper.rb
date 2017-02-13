f35fbecb0df8fe4032cc95c5faa87ad7c3f24972
Vasily Toropchin <vasview@gmail.com>
2017-02-13 17:38:44 +0600

    Добавил запис в rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'database_cleaner'
+require 'capybara/rspec'
 
 DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
