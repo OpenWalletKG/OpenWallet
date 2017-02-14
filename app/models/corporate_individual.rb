948446642106665b484e965db7e440332ad37156
Dmitrii <zagvozkin.dmitry@gmail.com>
2017-02-14 15:23:25 +0600

    #14 Исправление

--- app/models/corporate_individual.rb
+++ app/models/corporate_individual.rb
@@ -12,6 +12,7 @@ class CorporateIndividual < ApplicationRecord
 
   def self.is_employee?( individual_in )
     individual = Individual.find_by_in( individual_in )
+    return false if individual.nil?
     !!find_by( individual_id: individual.id )
   end
 
